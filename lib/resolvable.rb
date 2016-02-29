885d3f3cef4bd58351df3dfd3be13e0bcf43d89c
Alec Hipshear <alechipshear@gmail.com>
2016-02-28 20:18:13 -0500

    First pass at making this work
    - Resolvables can be marked as either success or failure, but not both.
    - Resolvables track basic error states
    - Resolvables prevent multiple calls to `success!` or changing from one
      state to another; multiple `failure!` calls is acceptable.

--- lib/resolvable.rb
+++ lib/resolvable.rb
@@ -1,5 +1,45 @@
 require "resolvable/version"
 
 module Resolvable
-  # Your code goes here...
+  class DoubleSuccessError < StandardError; end
+  class FailureAfterSuccessError < StandardError; end
+
+  module ClassMethods
+    def default_failure_message
+    "Failed to resolve #{self.name}"
+    end
+  end
+
+  def self.included(klass)
+    klass.extend(Resolvable::ClassMethods)
+  end
+
+  def success!
+    raise DoubleSuccessError.new if(defined? @success)
+
+    @success = true
+    self
+  end
+
+  def failure!
+    raise FailureAfterSuccessError.new if(success?)
+
+    @success = false
+    @errors ||= []
+    @errors << self.class.default_failure_message
+
+    self
+  end
+
+  def success?
+    @success == true
+  end
+
+  def failure?
+    @success == false
+  end
+
+  def errors
+    @errors
+  end
 end
--- spec/resolvable_spec.rb
+++ spec/resolvable_spec.rb
@@ -1,11 +1,54 @@
 require 'spec_helper'
 
 describe Resolvable do
+  class NeedsResolution; include Resolvable; end
+
   it 'has a version number' do
     expect(Resolvable::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'allows success' do
+    expect(NeedsResolution.new.success!).to be_success
+  end
+
+  it 'allows failure' do
+    expect(NeedsResolution.new.failure!).to be_failure
+  end
+
+  it 'is not successful if it failed' do
+    expect(NeedsResolution.new.failure!).to_not be_success
+  end
+
+  it 'is not a failure if it succeeded' do
+    expect(NeedsResolution.new.success!).to_not be_failure
+  end
+
+  it 'prevents double successes' do
+    needs_resolution = NeedsResolution.new
+    expect do
+      needs_resolution.success!
+      needs_resolution.success!
+    end.to raise_exception(Resolvable::DoubleSuccessError)
+  end
+
+  it 'prevents success after failure' do
+    needs_resolution = NeedsResolution.new
+    expect do
+      needs_resolution.success!
+      needs_resolution.failure!
+    end.to raise_exception(Resolvable::FailureAfterSuccessError)
+  end
+
+  it 'has error messages in the case of failure' do
+    needs_resolution = NeedsResolution.new
+    needs_resolution.failure!
+    expect(needs_resolution.errors).to eq([NeedsResolution.default_failure_message])
+  end
+
+  it 'allows double failures, and adds to the error stack' do
+    needs_resolution = NeedsResolution.new
+    needs_resolution.failure!
+    needs_resolution.failure!
+    expect(needs_resolution.errors).to eq([NeedsResolution.default_failure_message] * 2)
   end
 end
