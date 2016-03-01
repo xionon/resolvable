9bbb438a1332ecc7489b6c7f2731e178ad8df524
Alec Hipshear <alechipshear@gmail.com>
2016-02-29 23:32:03 -0500

    OpenStructShim includes Resolvable
    In order to be a proper shim, we need to include Resolvable, which
    provides a path forwards for replacing shim'd OpenStructs with regular
    POROs.

--- lib/resolvable/open_struct_shim.rb
+++ lib/resolvable/open_struct_shim.rb
@@ -2,6 +2,11 @@ require 'ostruct'
 
 module Resolvable
   class OpenStructShim < OpenStruct
+    include Resolvable
+    def self.default_failure_message
+      "Failed to resolve #{self.name}"
+    end
+
     attr_writer :kernel
 
     def method_missing(method_name, *args)
--- spec/resolvable_ostruct_shim_spec.rb
+++ spec/resolvable_ostruct_shim_spec.rb
@@ -25,4 +25,14 @@ describe Resolvable::OpenStructShim do
     expect(fake_kernel).to receive(:warn).with(expected_message)
     expect(subject.no_method_here).to eq(nil)
   end
+
+  context "responding like resolvable" do
+    it "responds to success!" do
+      expect(subject.success!).to be_success
+    end
+
+    it "responds to failure!" do
+      expect(subject.failure!).to be_failure
+    end
+  end
 end
