1e684d91bf785c9bff49031ccafc33693047146e
Alec Hipshear <alechipshear@gmail.com>
2016-03-22 10:12:54 -0400

    Suggest attr_accessor and attr_reader hooks for undefined methods

--- lib/resolvable/open_struct_shim.rb
+++ lib/resolvable/open_struct_shim.rb
@@ -1,24 +1,70 @@
 require 'ostruct'
 
 module Resolvable
+  SuggestedMethod = Struct.new(:file, :line, :method_info, :type)
+
   class OpenStructShim < OpenStruct
     include Resolvable
+
+    def self.warn=(should_warn)
+      @@warn = should_warn
+    end
+
+    def self.suggest=(should_suggest)
+      @@suggest = should_suggest
+    end
+
     def self.default_failure_message
       "Failed to resolve #{self.name}"
     end
 
-    attr_writer :kernel
+    attr_accessor :kernel
 
-    def method_missing(method_name, *args)
-      file, line, method_info = caller(1, 1)[0].split(":")
+    def initialize(*args)
+      auto_generated_methods = args[0] || {}
+      auto_generated_methods.each do |k, v| 
+        __suggest_method__("#{k}=", nil, nil, nil) if @@suggest
+      end
 
-      kernel.warn "Missing method called on OpenStructShim: #{method_name} is not defined on #{self.class.name} (Called from #{file}##{line} in #{method_info}"
+      self.kernel = Kernel
 
       super
     end
 
-    def kernel
-      @kernel ||= Kernel
+    def method_missing(method_name, *args)
+      file, line, method_info = caller(1, 1)[0].split(":")
+
+      __warn__(method_name, file, line, method_info) if @@warn
+      __suggest_method__(method_name, file, line, method_info) if @@suggest
+
+      super
+    end
+
+    def __warn__(method_name, file, line, method_info)
+      kernel.warn "Missing method called on OpenStructShim: #{method_name} is not defined on #{self.class.name} (Called from #{file}##{line} in #{method_info}"
+    end
+
+    def __suggest_method__(method_name, file, line, method_info)
+      type = :attr_reader
+
+      if method_name =~ /=$/
+        type = :attr_accessor
+        method_name = method_name.to_s.gsub(/=$/, "").to_sym
+      end
+
+      __suggested_methods__[method_name] = Resolvable::SuggestedMethod.new(file, line, method_info, type)
+    end
+
+    def __suggested_methods__
+      @__suggested_methods__ ||= {}
+    end
+
+    def __suggested_method_summary__
+      __suggested_methods__
+        .group_by { |key, suggestion| suggestion.type }
+        .map { |attr_type, suggestions|
+          "#{attr_type} #{suggestions.map { |s| ":#{s[0]}" }.join(", ")}"
+        }.join("\n")
     end
   end
 end
---
+++ spec/open_struct_shim_self_modifiying_spec.rb
@@ -0,0 +1,101 @@
+require "spec_helper"
+
+describe Resolvable::OpenStructShim do
+  let(:fake_warnings) { StringIO.new }
+  let(:fake_kernel) do
+    class_double(Kernel).tap do |kernel|
+      allow(kernel).to receive(:warn) { |msg| fake_warnings.puts(msg) }
+    end
+  end
+
+  context "with self.warn=true and self.suggest=true" do
+    class ResolveAndSuggest < Resolvable::OpenStructShim
+      self.warn = true
+      self.suggest = true
+      attr_reader :readable
+      attr_accessor :accessable
+      def calls_unexpected_method
+        self.suggested_method()
+      end
+    end
+
+    subject do
+      ResolveAndSuggest.new.tap do |nr|
+        nr.kernel = fake_kernel
+      end
+    end
+
+    it "suggests new methods to replace missing ones" do
+      subject.suggested_method
+      expect(subject.__suggested_methods__).to have_key(:suggested_method)
+    end
+
+    it "gives structured information about unexpected methods" do
+      subject.calls_unexpected_method
+      suggested_method = subject.__suggested_methods__[:suggested_method]
+
+      expect(suggested_method.file).to eq(__FILE__)
+      expect(suggested_method.line).to eq("18")
+      expect(suggested_method.method_info).to eq("in `calls_unexpected_method'")
+    end
+
+    it "doesn't warn on attr_reader methods" do
+      subject.readable
+      expect(subject.__suggested_methods__).to_not have_key(:readable)
+    end
+
+    it "doesn't warn on attr_accessor methods" do
+      subject.accessable = :foo
+      expect(subject.accessable).to eq(:foo)
+      expect(subject.__suggested_methods__).to be_empty
+    end
+
+    it "suggests using attr_reader for values that are only read" do
+      subject.unreadable
+      suggestion = subject.__suggested_methods__[:unreadable]
+      expect(suggestion.type).to eq(:attr_reader)
+    end
+
+    it "suggests using attr_accessor for values that are read and written" do
+      subject.unaccessable
+      subject.unaccessable = :foo
+      suggestion = subject.__suggested_methods__[:unaccessable]
+      expect(suggestion.type).to eq(:attr_accessor)
+    end
+
+    it "generats a list of suggested attrs" do
+      subject.reader_1
+      subject.reader_2
+      subject.accessor_1 = :foo
+
+      expect(subject.__suggested_method_summary__).to match("attr_reader :reader_1, :reader_2")
+      expect(subject.__suggested_method_summary__).to match("attr_accessor :accessor_1")
+    end
+  end
+
+  context "responding like a resolvable" do
+    subject do
+      ResolveAndSuggest.new(:foo => :bar, :success? => false).tap do |nr|
+        nr.kernel = fake_kernel
+      end
+    end
+
+    it "responds to success!" do
+      expect(subject).to respond_to(:success!)
+    end
+
+    it "can be marked as success using the resolvable method" do
+      expect { subject.success! }.to change { subject.success? }.to(true)
+    end
+
+    it "responds to methods sent during initialize" do
+      expect { subject.foo = :baz }.to change { subject.foo }.to(:baz)
+    end
+
+    it "adds methods set during initialize to the suggested methods list" do
+      expect { subject.foo }.to_not change { subject.__suggested_methods__ }
+      expect(subject.__suggested_methods__).to have_key(:foo)
+      expect(subject.__suggested_method_summary__).to match("attr_accessor :foo")
+    end
+  end
+end
